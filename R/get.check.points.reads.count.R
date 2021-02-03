a7bb2f044f6c2c5f8700535e6df804e806e4ae91
helen-zhu <helen.m.zhu@gmail.com>
2021-02-03 14:46:36 -0500

    testing

--- R/get.check.points.reads.count.R
+++ R/get.check.points.reads.count.R
@@ -9,14 +9,14 @@
 
 # get check point reads count
 .get.check.points.reads.count<- function(ibam,anno,bam,check_points,PARAMETERS){
-  
+
   # GENE
-  # print(paste0(anno$gene, "\n"))
-  
+  print(anno$gene)
+
   # RNA2DNA
   RNA2DNA = anno$left:anno$right
   RNA2DNA = RNA2DNA[anno$DNA2RNA > 0]
-  
+
   # Identifying GRanges
   .which = GenomicRanges::GRanges(seqnames = anno$chr, IRanges::IRanges(check_points, check_points), strand = anno$strand)
   .which = GenomicRanges::resize(.which, width = PARAMETERS$WINDOW_WIDTH, fix = "center")
@@ -25,7 +25,7 @@
   start(.which) = ifelse(start(.which) < 0, 1, start(.which))
   start(.which) = RNA2DNA[start(.which)]
   .which = S4Vectors::split(.which, 1:length(.which))
-  
+
   # Removing Introns
   if(length(which(anno$DNA2RNA == 0)) > 0){
     introns = GenomicRanges::GRanges(seqnames = anno$chr, IRanges::IRanges(which(anno$DNA2RNA == 0)+anno$left), strand = anno$strand)
@@ -35,7 +35,7 @@
     .which = GenomicRanges::setdiff(.which, introns.rep)
   }
   .which = unlist(.which)
-  
+
   # Determining which strand to use
   ID.strand = structure(c("+", "-"), names = c("-", "+"))
   if(PARAMETERS$STRANDED == "forward"){
@@ -47,80 +47,80 @@
   } else{
     ID.strand.m1 = ID.strand.m2 = ID.strand
   }
-  
+
   # Importing bam file
   if(PARAMETERS$PAIRED){
-    
+
     # Flags
     flag.m1 = scanBamFlag(isProperPair = T, isFirstMateRead = T)
     flag.m2 = scanBamFlag(isProperPair = T, isSecondMateRead = T)
-    
+
     # What
     what = c("qname", "strand", "pos", "mapq")
-    
+
     # Importing Bams
     param.m1 = ScanBamParam(which=.which, what=what, flag = flag.m1)
     bam.m1 <- scanBam(bam[ibam], param=param.m1)
-    
+
     param.m2 = ScanBamParam(which=.which, what=what, flag = flag.m2)
     bam.m2 <- scanBam(bam[ibam], param=param.m2)
-    
+
     bin.indices = names(.which)
     reads.names = list()
-    
+
     # Counting Unique Read IDs
     for(i in 1:length(.which)){
-      
+
       # First mates
       qname.m1 = bam.m1[[i]]$qname
       strand.m1 = bam.m1[[i]]$strand
       mapq.m1 = bam.m1[[i]]$mapq
       qname.m1 = qname.m1[strand.m1 %in% ID.strand.m1 & mapq.m1 >= 255]
-      
+
       # Second mates
       qname.m2 = bam.m2[[i]]$qname
       strand.m2 = bam.m2[[i]]$strand
       mapq.m2 = bam.m2[[i]]$mapq
       qname.m2 = qname.m2[strand.m2 %in% ID.strand.m2 & mapq.m2 >= 255]
-      
+
       reads.names[[bin.indices[i]]] = unique(c(reads.names[[bin.indices[i]]], qname.m1, qname.m2))
     }
-    
-    
+
+
   } else { # PARAMETERS$PAIRED = FALSE
-    
+
     # Flag
     flag = scanBamFlag()
-    
+
     # What
     what = c("qname", "strand", "pos", "mapq")
-    
+
     # Importing Bams
     param = ScanBamParam(which=.which, what=what, flag = flag)
     bam.m1 <- scanBam(bam[ibam], param=param)
-    
+
     bin.indices = names(.which)
     reads.names = list()
-    
+
     # Counting Unique Read IDs
     for(i in 1:length(.which)){
-      
+
       # First mates
       qname.m1 = bam.m1[[i]]$qname
       strand.m1 = bam.m1[[i]]$strand
       mapq.m1 = bam.m1[[i]]$mapq
       qname.m1 = qname.m1[strand.m1 %in% ID.strand.m1 & mapq.m1 >= 255]
-      
+
       reads.names[[bin.indices[i]]] = unique(c(reads.names[[bin.indices[i]]], qname.m1))
     }
-    
+
   }
-  
+
   check_points_count = unlist(lapply(reads.names, length))
   names(check_points_count) = check_points
-  
+
   if (PARAMETERS$REMOVE_LOCAL_TAG_ANOMALITIES==TRUE) {check_points_count=.remove.local.anomalities(check_points_count)}
-  
+
   # return result
   return(check_points_count)
 }
